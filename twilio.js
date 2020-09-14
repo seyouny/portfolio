3df5bf4c18c00504d60228dd27f429f95c664bb1
Seyoung Yoon <stacey.exp@gmail.com>
2020-09-14 15:31:50 -0700

    twilio removed

--- twilio.js
+++ twilio.js
@@ -1,15 +1,15 @@
 
 // require('dotenv').config();
 
-const accountSid = 'AC2bcc4f0559b9a94e2bcb18fe4b9de526';
-const authToken = '93c4087f8b514649229160596303e1bf';
+const accountSid = ;
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 const sendSms = (phone, message) => {
   client.messages
     .create({
        body: message,
-       from: '+12059463196',
+       from: '+',
        to: '+1'+phone
      })
     .then(message => console.log(message.sid));
